3f7bbc6ce28816401aa35616a10dd7f0fbccf63f
Sébastien Crozet <developer@crozet.re>
2013-05-19 19:46:42 +0000

    plane vs implicit shape now takes in account the plane center.

--- src/narrow/plane_implicit.rs
+++ src/narrow/plane_implicit.rs
@@ -1,18 +1,20 @@
-use core::num::{Zero, One};
+use core::num::Zero;
 use nalgebra::traits::dot::{Dot};
-use nalgebra::traits::workarounds::scalar_op::{ScalarMul, ScalarDiv};
+use nalgebra::traits::vector_space::VectorSpace;
+use nalgebra::traits::ring::Ring;
+use nalgebra::traits::workarounds::scalar_op::ScalarMul;
 use geom::plane::Plane;
 use geom::implicit::Implicit;
 use contact::Contact;
 
 pub fn update_collide_plane_implicit_shape<
-         T: Zero + One + Ord + Add<T, T> + Copy,
-         V: Dot<T> + Neg<V> + Add<V, V> + ScalarMul<T> + ScalarDiv<T> + Copy,
+         V: VectorSpace<T> + Dot<T> + Copy,
+         T: Ring + Ord + Copy,
          G: Implicit<V>>
-   (plane: Plane<V>, other: G, out: &mut Contact<V, T>) -> bool
+   (center: &V, plane: &Plane<V>, other: &G, out: &mut Contact<V, T>) -> bool
 {
   let deepest = &other.support_point(&-plane.normal);
-  let dist    = &plane.normal.dot(&-deepest);     // FIXME: handle the plane center
+  let dist    = &plane.normal.dot(&(*center - *deepest));
 
   if (*dist > Zero::zero())
   {
@@ -26,10 +28,10 @@ pub fn update_collide_plane_implicit_shape<
 }
 
 pub fn collide_plane_implicit_shape<
-         T: Zero + One + Ord + Add<T, T> + Copy,
-         V: Zero + Dot<T> + Neg<V> + Add<V, V> + ScalarMul<T> + ScalarDiv<T> + Copy,
+         V: VectorSpace<T> + Dot<T> + Copy,
+         T: Ring + Ord + Copy,
          G: Implicit<V>>
-   (plane: Plane<V>, other: G) -> Option<~Contact<V, T>>
+   (center: &V, plane: &Plane<V>, other: &G) -> Option<~Contact<V, T>>
 {
   let mut res = 
     ~Contact { world_contact1: Zero::zero(),
@@ -38,7 +40,7 @@ pub fn collide_plane_implicit_shape<
                normal:         Zero::zero(),
                depth:          Zero::zero() };
 
-  if (update_collide_plane_implicit_shape(plane, other, res))
+  if (update_collide_plane_implicit_shape(center, plane, other, res))
   { Some(res) }
   else
   { None }
