093d054448eedfac336f598ba933470d887e2143
Sébastien Crozet <developer@crozet.re>
2013-08-05 16:38:37 +0200

    There is no reason for the dispatcher to take an @mut instead of a &.

--- src/broad/dispatcher.rs
+++ src/broad/dispatcher.rs
@@ -1,23 +1,23 @@
-use std::managed;
+use std::borrow;
 
 /// Trait of dispatcher. A (collision) dispatcher typically takes two bodies in parameter and
 /// return the corresponding narrow phase algorithm.
 pub trait Dispatcher<B, NF> {
     /// Deduce the narrow phase from two bodies.
-    fn dispatch(&self, @mut B, @mut B) -> NF;
+    fn dispatch(&self, &B, &B) -> NF;
     /// Tells whether a collision between two bodies can occur.
-    fn is_valid(&self, @mut B, @mut B) -> bool;
+    fn is_valid(&self, &B, &B) -> bool;
 }
 
 /// Dispatcher which disallows dispatches between identical pointers. It has no result.
 pub struct NoIdDispatcher<B>;
 
 impl<B> Dispatcher<B, ()> for NoIdDispatcher<B> {
-    fn dispatch(&self, _: @mut B, _: @mut B) -> () {
+    fn dispatch(&self, _: &B, _: &B) -> () {
         ()
     }
 
-    fn is_valid(&self, a: @mut B, b: @mut B) -> bool {
-        !managed::mut_ptr_eq(a, b)
+    fn is_valid(&self, a: &B, b: &B) -> bool {
+        !borrow::ref_eq(a, b)
     }
 }
