1b2650945e4ab0964e501e769f10e53da7b6ce0c
Sébastien Crozet <developer@crozet.re>
2014-01-03 10:12:49 +0100

    Add uv mapping for boxes and aabbs.

--- src/ray/ray_aabb.rs
+++ src/ray/ray_aabb.rs
@@ -3,10 +3,10 @@ use std::util;
 use nalgebra::na::Indexable;
 use nalgebra::na;
 use ray::{Ray, RayCast};
-use bounding_volume;
+use bounding_volume::AABB;
 use math::{N, V};
 
-impl RayCast for bounding_volume::AABB {
+impl RayCast for AABB {
     fn toi_with_ray(&self, ray: &Ray) -> Option<N> {
         let mut tmin: N = na::zero();
         let mut tmax: N = Bounded::max_value();
@@ -40,66 +40,89 @@ impl RayCast for bounding_volume::AABB {
     }
 
     fn toi_and_normal_with_ray(&self, ray: &Ray) -> Option<(N, V)> {
-        let mut tmax: N = Bounded::max_value();
-        let mut tmin: N = -tmax;
-        let mut side = 0;
-        let mut diag = false;
+        ray_aabb(self, ray).map(|(t, n, _)| (t, n))
+    }
 
-        for i in range(0u, na::dim::<V>()) {
-            if ray.dir.at(i).is_zero() {
-                if ray.orig.at(i) < self.mins().at(i) || ray.orig.at(i) > self.maxs().at(i) {
-                    return None
-                }
+    #[cfg(dim3)]
+    fn toi_and_normal_and_uv_with_ray(&self, ray: &Ray) -> Option<(N, V, Option<(N, N, N)>)> {
+        ray_aabb(self, ray).map(|(t, n, s)| {
+            let pt  = ray.orig + ray.dir * t;
+            let lpt = (pt - *self.mins()) / (self.maxs() - *self.mins());
+            let id  = s.abs();
+
+            if id == 1 {
+                (t, n, Some((lpt.y.clone(), lpt.z.clone(), na::zero())))
+            }
+            else if id == 2 {
+                (t, n, Some((lpt.z.clone(), lpt.x.clone(), na::zero())))
             }
             else {
-                let _1: N = na::one();
-                let denom = _1 / ray.dir.at(i);
-                let flip_sides;
-                let mut inter_with_near_plane = (self.mins().at(i) - ray.orig.at(i)) * denom;
-                let mut inter_with_far_plane  = (self.maxs().at(i) - ray.orig.at(i)) * denom;
-
-                if inter_with_near_plane > inter_with_far_plane {
-                    flip_sides = true;
-                    util::swap(&mut inter_with_near_plane, &mut inter_with_far_plane)
-                }
-                else {
-                    flip_sides = false;
-                }
-
-                if inter_with_near_plane > tmin {
-                    tmin = inter_with_near_plane;
-                    side = if flip_sides { -(i as int + 1) } else { i as int + 1 };
-                    diag = false;
-                }
-                else if inter_with_near_plane == tmin {
-                    diag = true;
-                }
-
-                tmax = tmax.min(&inter_with_far_plane);
-
-                if tmin > tmax {
-                    return None;
-                }
+                (t, n, Some((lpt.x.clone(), lpt.y.clone(), na::zero())))
             }
-        }
+        })
+    }
+}
 
-        if tmin < na::cast(0.0) {
-            return None;
-        }
+fn ray_aabb(aabb: &AABB, ray: &Ray) -> Option<(N, V, int)> {
+    let mut tmax: N = Bounded::max_value();
+    let mut tmin: N = -tmax;
+    let mut side = 0;
+    let mut diag = false;
 
-        if diag {
-            Some((tmin, -na::normalize(&ray.dir)))
+    for i in range(0u, na::dim::<V>()) {
+        if ray.dir.at(i).is_zero() {
+            if ray.orig.at(i) < aabb.mins().at(i) || ray.orig.at(i) > aabb.maxs().at(i) {
+                return None
+            }
         }
         else {
-            let mut normal: V = na::zero();
+            let _1: N = na::one();
+            let denom = _1 / ray.dir.at(i);
+            let flip_sides;
+            let mut inter_with_near_plane = (aabb.mins().at(i) - ray.orig.at(i)) * denom;
+            let mut inter_with_far_plane  = (aabb.maxs().at(i) - ray.orig.at(i)) * denom;
 
-            if side < 0 {
-                normal.set((-side - 1) as uint, na::one::<N>());
+            if inter_with_near_plane > inter_with_far_plane {
+                flip_sides = true;
+                util::swap(&mut inter_with_near_plane, &mut inter_with_far_plane)
             }
             else {
-                normal.set((side - 1) as uint, -na::one::<N>());
+                flip_sides = false;
             }
-            Some((tmin, normal))
+
+            if inter_with_near_plane > tmin {
+                tmin = inter_with_near_plane;
+                side = if flip_sides { -(i as int + 1) } else { i as int + 1 };
+                diag = false;
+            }
+            else if inter_with_near_plane == tmin {
+                diag = true;
+            }
+
+            tmax = tmax.min(&inter_with_far_plane);
+
+            if tmin > tmax {
+                return None;
+            }
         }
+    }
+
+    if tmin < na::cast(0.0) {
+        return None;
+    }
+
+    if diag {
+        Some((tmin, -na::normalize(&ray.dir), side))
+    }
+    else {
+        let mut normal: V = na::zero();
+
+        if side < 0 {
+            normal.set((-side - 1) as uint, na::one::<N>());
+        }
+        else {
+            normal.set((side - 1) as uint, -na::one::<N>());
+        }
+        Some((tmin, normal, side))
     }
 }
--- src/ray/ray_box.rs
+++ src/ray/ray_box.rs
@@ -36,6 +36,21 @@ impl RayCast for Box {
                 ray)
         }
     }
+
+    #[cfg(dim3)]
+    #[inline]
+    fn toi_and_normal_and_uv_with_ray(&self, ray: &Ray) -> Option<(N, V, Option<(N, N, N)>)> {
+        if self.margin().is_zero() {
+            AABB::new(-self.half_extents(), self.half_extents()).toi_and_normal_and_uv_with_ray(ray)
+        }
+        else {
+            gjk_toi_and_normal_with_ray(
+                &Identity::new(),
+                self,
+                &mut JohnsonSimplex::<V>::new_w_tls(),
+                ray).map(|(t, n)| (t, n, None))
+        }
+    }
 }
 
 impl RayCastWithTransform for Box { }
