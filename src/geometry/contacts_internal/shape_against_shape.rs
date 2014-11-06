f2f53fecc19827a0f473f3342ad0688dabbe9733
Sébastien Crozet <developer@crozet.re>
2014-11-06 20:06:35 +0100

    Fix comments for `shapes_against_shapes` and `manifold_shapes_against_shapes`.

--- src/geometry/contacts_internal/shape_against_shape.rs
+++ src/geometry/contacts_internal/shape_against_shape.rs
@@ -1,6 +1,5 @@
 use std::intrinsics::TypeId;
 use std::any::AnyRefExt;
-use std::default::Default;
 use na::{Translate, Rotation, Cross};
 use geometry::{Contact, contact, contacts};
 use shape::{Shape, Ball, Plane, Cuboid, Capsule, Cone, Cylinder, Convex, Compound, Mesh, Segment,
@@ -23,32 +22,44 @@ macro_rules! dispatch_part(
     }
 )
 
-macro_rules! dispatch(
-    ($dispatcher_name: ident, $fname: ident $(, $args: ident: $types: ty)* -> $result: ty) => {
-        /// FIXME
-        #[inline]
-        pub fn $dispatcher_name<N, P, V, AV, M, I>(m1: &M, g1: &Shape<N, P, V, M>,
-                                                   m2: &M, g2: &Shape<N, P, V, M>
-                                                   $(, $args: $types)*)
-                                                   -> $result
-            where N:  Scalar,
-                  P:  Point<N, V>,
-                  V:  Vect<N> + Translate<P> + HasInertiaMatrix<I> + Cross<AV>,
-                  AV: Vect<N>,
-                  M:  Isometry<N, P, V> + Rotation<AV>,
-                  I:  Send + Sync + Clone {
-            let tg1 = g1.get_type_id();
-            let tg2 = g2.get_type_id();
+/// Best contact between two shapes (trait objects).
+#[inline]
+pub fn shape_against_shape<N, P, V, AV, M, I>(m1: &M, g1: &Shape<N, P, V, M>,
+                                              m2: &M, g2: &Shape<N, P, V, M>,
+                                              prediction: N)
+                                              -> Option<Contact<N, P, V>>
+    where N:  Scalar,
+          P:  Point<N, V>,
+          V:  Vect<N> + Translate<P> + HasInertiaMatrix<I> + Cross<AV>,
+          AV: Vect<N>,
+          M:  Isometry<N, P, V> + Rotation<AV>,
+          I:  Send + Sync + Clone {
+    let tg1 = g1.get_type_id();
+    let tg2 = g2.get_type_id();
 
-            // FIXME: use a hash-map instead of if-elses ?
+    // FIXME: use a hash-map instead of if-elses ?
+    apply_to_all_shape_pair!(dispatch_part, contact, m1, g1, tg1, m2, g2, tg2, prediction)
+
+    None
+}
 
 
-            apply_to_all_shape_pair!(dispatch_part, $fname, m1, g1, tg1, m2, g2, tg2$(, $args)*)
 
-            Default::default()
-        }
-    }
-)
+/// Contacts between two shapes (trait objects).
+#[inline]
+pub fn manifold_shape_against_shape<N, P, V, AV, M, I>(m1: &M, g1: &Shape<N, P, V, M>,
+                                                       m2: &M, g2: &Shape<N, P, V, M>,
+                                                       prediction: N,
+                                                       out: &mut Vec<Contact<N, P, V>>)
+    where N:  Scalar,
+          P:  Point<N, V>,
+          V:  Vect<N> + Translate<P> + HasInertiaMatrix<I> + Cross<AV>,
+          AV: Vect<N>,
+          M:  Isometry<N, P, V> + Rotation<AV>,
+          I:  Send + Sync + Clone {
+    let tg1 = g1.get_type_id();
+    let tg2 = g2.get_type_id();
 
-dispatch!(shape_against_shape, contact, prediction: N -> Option<Contact<N, P, V>>)
-dispatch!(manifold_shape_against_shape, contacts, prediction: N, out: &mut Vec<Contact<N, P, V>> -> ())
+    // FIXME: use a hash-map instead of if-elses ?
+    apply_to_all_shape_pair!(dispatch_part, contacts, m1, g1, tg1, m2, g2, tg2, prediction, out)
+}
