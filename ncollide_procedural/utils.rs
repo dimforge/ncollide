de5deb818810a21adea0892b98e95c9f8f1663dc
Ben Foppa <benjamin.foppa@gmail.com>
2015-01-10 13:01:28 -0500

    Update ncollide_procedural for rustc 1.0 alpha

--- ncollide_procedural/utils.rs
+++ ncollide_procedural/utils.rs
@@ -165,7 +165,7 @@ pub fn split_index_buffer_and_recover_topology<P: PartialEq + AsBytes + Clone>(
                         new_coords: &mut Vec<P>)
                         -> u32 {
         let key = unsafe { HashablePartialEq::new(coord.clone()) };
-        let id = match vtx_to_id.entry(&key) {
+        let id = match vtx_to_id.entry(key) {
             Entry::Occupied(entry) => entry.into_mut(),
             Entry::Vacant(entry)   => entry.insert(new_coords.len() as u32)
         };
