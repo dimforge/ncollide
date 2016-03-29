13d8e6f97d9cf747c7c71e306660aac7280678e1
Sébastien Crozet <developer@crozet.re>
2016-03-29 22:07:13 +0200

    Improve collision groups.
    See the comments for the `CollisionGroups` structure for more details on the change.

--- ncollide_pipeline/world/collision_groups.rs
+++ ncollide_pipeline/world/collision_groups.rs
@@ -1,13 +1,44 @@
 const SELF_COLLISION: u32 = 1 << 31;
 const ALL_GROUPS: u32 = (1 << 30) - 1;
+const NO_GROUP: u32 = 0;
 
-/// 30 groups of collision used to filter which object collide with which other one.
+/// Groups of collision used to filter which object collide with which other one.
 ///
-/// If two objects have at least one collision group in common, they can collide. Otherwise, they
-/// cannot.
+/// There are at most 30 groups indexed from 0 to 29 (included). This identifies collidable
+/// entities by combining three attributes: 
+///    * A set of group this structure is member of.
+///    * A collision group whitelist.
+///    * A collision group blacklist.
+///
+/// For two entities to collide, they must be member of at least one group part of each-other's
+/// whitelists, and must not be part of any blacklisted group. The blacklist always has priority on
+/// the whitelist.
+///
+/// ### Example
+/// For example if the object A is such that:
+///    * It is part of the groups 1, 3, and 6.
+///    * It whitelists the groups 3 and 7.
+///    * It blacklists the group 1.
+/// Let the object B be such that:
+///    * It is part of the groups 1, 3, and 7.
+///    * It whitelists the groups 3 and 7.
+///    * It does not blacklist anything.
+/// For example if the object C is such that:
+///    * It is part of the groups 6, 9.
+///    * It whitelists the groups 3 and 7.
+///    * It does not blacklist anything.
+///
+/// Then we have:
+///    * A and C can collide because A whitelists the group 6 (which C is part of), and,
+///    reciprocally, C whitelists the group 3 (which A is part of).
+///    * A and B will **not** collide because B is part of the group 1 which is blacklisted by A.
+///    * Finally, B and C will **not** collide either because, even if C whitelists the group 3
+///    (which B is part of), B does not whitelists the groups 6 nor 9 (which B is part of).
 #[derive(RustcEncodable, RustcDecodable, Clone, Debug)]
 pub struct CollisionGroups {
-    tag: u32
+    membership: u32,
+    whitelist:  u32,
+    blacklist:  u32,
 }
 
 impl CollisionGroups {
@@ -16,99 +47,100 @@ impl CollisionGroups {
     #[inline]
     pub fn new() -> CollisionGroups {
         CollisionGroups {
-            tag: ALL_GROUPS
+            membership: ALL_GROUPS,
+            whitelist:  ALL_GROUPS,
+            blacklist:  NO_GROUP
         }
     }
 
-    /// Creates a new `CollisionGroups` that enables collisions with the given groups.
-    ///
-    /// This does not enable self-collision.
     #[inline]
-    pub fn new_with_groups(groups: &[usize]) -> CollisionGroups {
-        let mut res = CollisionGroups { tag: 0 };
-
-        for group in groups.iter() {
-            res.enable_collisions_with_group(*group)
-        }
-
-        res
-    }
-
-    /// Creates a new `CollisionGroups` that enables collisions with every group but the given
-    /// ones.
-    ///
-    /// This does not enable self-collision.
-    #[inline]
-    pub fn new_without_groups(groups: &[usize]) -> CollisionGroups {
-        let mut res = CollisionGroups { tag: ALL_GROUPS };
-
-        for group in groups.iter() {
-            res.disable_collisions_with_group(*group)
-        }
-
-        res
-    }
-
-    /// Enables collision with all groups.
-    ///
-    /// This does not affect self-collision.
-    #[inline]
-    pub fn enable_collisions_with_all_groups(&mut self) {
-        self.tag = self.tag | ALL_GROUPS;
-    }
-
-    /// Disables collision with all groups.
-    ///
-    /// This does not affect self-collision.
-    #[inline]
-    pub fn disable_collisions_with_all_groups(&mut self) {
-        self.tag = self.tag & !ALL_GROUPS;
-    }
-
-    /// Enable collision detection with a specific group.
-    #[inline]
-    pub fn enable_collisions_with_group(&mut self, group_id: usize) {
+    fn modify_mask(mask: &mut u32, group_id: usize, add: bool) {
         assert!(group_id < 30, "There are at most 30 groups indexed from 0 to 29 (included).");
 
-        self.tag = self.tag | (1 << group_id);
+        if add {
+            *mask = *mask | (1 << group_id)
+        }
+        else {
+            *mask = *mask & !(1 << group_id)
+        }
     }
 
-    /// Disables collision detection with a specific group.
+    /// Adds or removes this entity from the given group.
     #[inline]
-    pub fn disable_collisions_with_group(&mut self, group_id: usize) {
-        assert!(group_id < 30, "There are at most 30 groups indexed from 0 to 29 (included).");
+    pub fn modify_membership(&mut self, group_id: usize, add: bool) {
+        CollisionGroups::modify_mask(&mut self.membership, group_id, add);
+    }
 
-        self.tag = self.tag & !(1 << group_id);
+    /// Adds or removes the given group from this entity whitelist.
+    #[inline]
+    pub fn modify_whitelist(&mut self, group_id: usize, add: bool) {
+        CollisionGroups::modify_mask(&mut self.whitelist, group_id, add);
+    }
+
+    /// Adds or removes this entity from the given group.
+    #[inline]
+    pub fn modify_blacklist(&mut self, group_id: usize, add: bool) {
+        CollisionGroups::modify_mask(&mut self.blacklist, group_id, add);
     }
 
     /// Enables self collision detection.
     #[inline]
     pub fn enable_self_collision(&mut self) {
-        self.tag = self.tag | SELF_COLLISION;
+        self.whitelist = self.whitelist | SELF_COLLISION;
     }
 
     /// Disables self collision detection.
     #[inline]
     pub fn disable_self_collision(&mut self) {
-        self.tag = self.tag & !SELF_COLLISION;
+        self.whitelist = self.whitelist & !SELF_COLLISION;
     }
 
-    /// Tests whether collisions with a given group is enabled.
     #[inline]
-    pub fn can_collide_with_group(&self, group_id: usize) -> bool {
+    fn is_inside_mask(mask: u32, group_id: usize) -> bool {
         assert!(group_id < 30, "There are at most 30 groups indexed from 0 to 29 (included).");
-        self.tag & (1 << group_id) != 0
+        mask & (1 << group_id) != 0
+    }
+
+    /// Tests if this entity is part of the given group.
+    #[inline]
+    pub fn is_member_of(&self, group_id: usize) -> bool {
+        CollisionGroups::is_inside_mask(self.membership, group_id)
+    }
+
+    /// Tests if the given group is whitelisted.
+    #[inline]
+    pub fn is_group_whitelisted(&self, group_id: usize) -> bool {
+        CollisionGroups::is_inside_mask(self.whitelist, group_id)
+    }
+
+    /// Tests if the given group is blacklisted.
+    #[inline]
+    pub fn is_group_blacklisted(&self, group_id: usize) -> bool {
+        CollisionGroups::is_inside_mask(self.blacklist, group_id)
+    }
+
+    /// Tests whether collisions with a given group is possible.
+    ///
+    /// Collision is possible if `group_id` is whitelisted but not blacklisted.
+    #[inline]
+    pub fn can_collide_with(&self, group_id: usize) -> bool {
+        !CollisionGroups::is_inside_mask(self.blacklist, group_id) &&
+        CollisionGroups::is_inside_mask(self.whitelist, group_id)
     }
 
     /// Tests whether two collision groups have at least one group in common.
     #[inline]
     pub fn can_collide_with_groups(&self, other: &CollisionGroups) -> bool {
-        (self.tag & other.tag & ALL_GROUPS) != 0
+        // FIXME: is there a more bitwise-y way of doing this?
+        self.membership  & other.blacklist == 0 &&
+        other.membership & self.blacklist  == 0 &&
+        self.membership  & other.whitelist != 0 &&
+        other.membership & self.whitelist  != 0
     }
 
     /// Tests whether self-collision is enabled.
     #[inline]
     pub fn can_collide_with_self(&self) -> bool {
-        self.tag & SELF_COLLISION != 0
+        self.whitelist & SELF_COLLISION != 0
     }
 }
