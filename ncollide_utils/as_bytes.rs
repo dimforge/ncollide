638169d49fd30ecd82aa03a8f1007bf1729bbb59
Sébastien Crozet <developer@crozet.re>
2016-04-02 21:33:59 +0200

    Implement `AsBytes` for Vec{2,3} and Pnt{2,3} with generic Copy content.

--- ncollide_utils/as_bytes.rs
+++ ncollide_utils/as_bytes.rs
@@ -1,4 +1,5 @@
 use std::mem;
+use std::slice;
 use na::{Vec2, Vec3, Pnt2, Pnt3};
 
 /// Trait that transforms thing to a slice of u8.
@@ -6,76 +7,22 @@ pub trait AsBytes {
     fn as_bytes<'a>(&'a self) -> &'a [u8];
 }
 
-impl AsBytes for Vec3<f32> {
-    #[inline(always)]
-    fn as_bytes<'a>(&'a self) -> &'a [u8] {
-        unsafe {
-            &mem::transmute::<&'a Vec3<f32>, &'a [u8; 12]>(self)[..]
+macro_rules! generic_as_bytes_impl(
+    ($t: ident, $dim: expr) => (
+        impl<N: Copy> AsBytes for $t<N> {
+            #[inline(always)]
+            fn as_bytes<'a>(&'a self) -> &'a [u8] {
+                unsafe {
+                    slice::from_raw_parts(mem::transmute(self), mem::size_of::<N>() * $dim)
+                }
+            }
         }
-    }
-}
+    )
+);
 
-impl AsBytes for Vec3<f64> {
-    #[inline(always)]
-    fn as_bytes<'a>(&'a self) -> &'a [u8] {
-        unsafe {
-            &mem::transmute::<&'a Vec3<f64>, &'a [u8; 24]>(self)[..]
-        }
-    }
-}
+generic_as_bytes_impl!(Vec2, 2);
+generic_as_bytes_impl!(Pnt2, 2);
+generic_as_bytes_impl!(Vec3, 2);
+generic_as_bytes_impl!(Pnt3, 2);
 
-impl AsBytes for Vec2<f32> {
-    #[inline(always)]
-    fn as_bytes<'a>(&'a self) -> &'a [u8] {
-        unsafe {
-            &mem::transmute::<&'a Vec2<f32>, &'a [u8; 8]>(self)[..]
-        }
-    }
-}
-
-impl AsBytes for Vec2<f64> {
-    #[inline(always)]
-    fn as_bytes<'a>(&'a self) -> &'a [u8] {
-        unsafe {
-            &mem::transmute::<&'a Vec2<f64>, &'a [u8; 16]>(self)[..]
-        }
-    }
-}
-
-impl AsBytes for Pnt3<f32> {
-    #[inline(always)]
-    fn as_bytes<'a>(&'a self) -> &'a [u8] {
-        unsafe {
-            &mem::transmute::<&'a Pnt3<f32>, &'a [u8; 12]>(self)[..]
-        }
-    }
-}
-
-impl AsBytes for Pnt3<f64> {
-    #[inline(always)]
-    fn as_bytes<'a>(&'a self) -> &'a [u8] {
-        unsafe {
-            &mem::transmute::<&'a Pnt3<f64>, &'a [u8; 24]>(self)[..]
-        }
-    }
-}
-
-impl AsBytes for Pnt2<f32> {
-    #[inline(always)]
-    fn as_bytes<'a>(&'a self) -> &'a [u8] {
-        unsafe {
-            &mem::transmute::<&'a Pnt2<f32>, &'a [u8; 8]>(self)[..]
-        }
-    }
-}
-
-impl AsBytes for Pnt2<f64> {
-    #[inline(always)]
-    fn as_bytes<'a>(&'a self) -> &'a [u8] {
-        unsafe {
-            &mem::transmute::<&'a Pnt2<f64>, &'a [u8; 16]>(self)[..]
-        }
-    }
-}
-
-// FIXME: implement for other things.
+// FIXME: implement for all `T: Copy` insead?
