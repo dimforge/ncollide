3802622d0c1b4c13589550eee59ddf0ea82e5127
Sébastien Crozet <developer@crozet.re>
2018-03-09 22:43:02 +0100

    Add closest points computation between two lines.

---
+++ ncollide_geometry/query/closest_points_internal/line_against_line.rs
@@ -0,0 +1,55 @@
+use approx::ApproxEq;
+use na;
+
+use math::Point;
+
+// FIXME: can we re-used this for the segment/segment case?
+/// Closest points between two segments.
+#[inline]
+pub fn line_against_line<P: Point>(
+    orig1: &P,
+    dir1: &P::Vector,
+    orig2: &P,
+    dir2: &P::Vector
+) -> (P, P) {
+    // Inspired by Real-time collision detection by Christer Ericson.
+    let r = *orig1 - *orig2;
+
+    let a = na::norm_squared(dir1);
+    let e = na::norm_squared(dir2);
+    let f = na::dot(dir2, &r);
+
+    let _0: P::Real = na::zero();
+    let _1: P::Real = na::one();
+
+    let mut s;
+    let mut t;
+
+    let _eps = P::Real::default_epsilon();
+    if a <= _eps && e <= _eps {
+        s = _0;
+        t = _0;
+    } else if a <= _eps {
+        s = _0;
+        t = f / e;
+    } else {
+        let c = na::dot(dir1, &r);
+        if e <= _eps {
+            t = _0;
+            s = -c / a;
+        } else {
+            let b = na::dot(dir1, dir2);
+            let denom = a * e - b * b;
+
+            if denom != _0 {
+                s = (b * f - c * e) / denom;
+            } else {
+                s = _0;
+            }
+
+            t = (b * s + f) / e;
+        }
+    }
+
+    (*orig1 + *dir1 * s, *orig2 + *dir2 * t)
+}
--- ncollide_geometry/query/closest_points_internal/mod.rs
+++ ncollide_geometry/query/closest_points_internal/mod.rs
@@ -4,6 +4,7 @@ pub use self::closest_points::ClosestPoints;
 pub use self::ball_against_ball::ball_against_ball;
 pub use self::segment_against_segment::{segment_against_segment,
                                         segment_against_segment_with_locations};
+pub use self::line_against_line::line_against_line;
 pub use self::support_map_against_support_map::support_map_against_support_map;
 pub use self::support_map_against_support_map::support_map_against_support_map_with_params;
 pub use self::plane_against_support_map::{plane_against_support_map, support_map_against_plane};
@@ -14,6 +15,7 @@ pub use self::composite_shape_against_shape::{composite_shape_against_shape,
 mod closest_points;
 mod ball_against_ball;
 mod segment_against_segment;
+mod line_against_line;
 mod support_map_against_support_map;
 mod plane_against_support_map;
 mod shape_against_shape;
