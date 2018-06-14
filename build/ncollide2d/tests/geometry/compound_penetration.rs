573ed72d309b5d55bebf26810783a8dad83584b0
sebcrozet <developer@crozet.re>
2018-06-14 15:45:09 +0200

    Take local compound part transform into account for query::contact computation.
    Fix #171.

---
+++ build/ncollide2d/tests/geometry/compound_penetration.rs
@@ -0,0 +1,42 @@
+use na::{self, Isometry2, Vector2};
+
+use ncollide2d::query;
+use ncollide2d::shape::{Compound, Cuboid, ShapeHandle};
+
+#[test]
+fn normal() {
+  let c1 = Cuboid::new(Vector2::new(1.0, 1.0));
+  let c2 = Cuboid::new(Vector2::new(1.0, 1.0));
+
+  let contact = query::contact(
+    &Isometry2::new(Vector2::new(10.5, 10.5), na::zero()),
+    &c2,
+    &Isometry2::new(Vector2::new(10.0, 10.0), na::zero()),
+    &c1,
+    1.0,
+  );
+
+  assert!(contact.is_some()); // ok
+}
+
+#[test]
+fn compound() {
+  let c1 = Cuboid::new(Vector2::new(1.0, 1.0));
+  let c2 = Cuboid::new(Vector2::new(1.0, 1.0));
+
+  let shapes = vec![(
+    Isometry2::new(Vector2::new(10.0, 10.0), na::zero()),
+    ShapeHandle::new(c1),
+  )];
+  let compound = Compound::new(shapes);
+
+  let contact = query::contact(
+    &Isometry2::new(Vector2::new(10.5, 10.5), na::zero()),
+    &c2,
+    &Isometry2::identity(),
+    &compound,
+    1.0,
+  );
+
+  assert!(contact.is_some()); // FAILED
+}
