5d41b02b666e34d7e7695fe05309133186bff9a8
sebcrozet <developer@crozet.re>
2018-06-14 16:56:21 +0200

    Add regression test for #157.

---
+++ build/ncollide3d/tests/geometry/cylinder_cuboid_contact.rs
@@ -0,0 +1,28 @@
+use na::{self, Isometry3, Vector3};
+use ncollide3d::query::{self, Proximity};
+use ncollide3d::shape::{Cuboid, Cylinder};
+
+// Issue #157.
+#[test]
+fn cylinder_cuboid_contact() {
+    let cyl = Cylinder::new(0.925, 0.5);
+    let cyl_at = Isometry3::new(Vector3::new(10.97, 0.925, 61.02), na::zero());
+    let cuboid = Cuboid::new(Vector3::new(0.05, 0.75, 0.5));
+    let cuboid_at = Isometry3::new(Vector3::new(11.50, 0.75, 60.5), na::zero());
+
+    let distance = query::distance_internal::support_map_against_support_map(
+        &cyl_at, &cyl, &cuboid_at, &cuboid,
+    );
+
+    let proximity = query::proximity_internal::support_map_against_support_map(
+        &cyl_at, &cyl, &cuboid_at, &cuboid, 0.1,
+    );
+
+    let contact = query::contacts_internal::support_map_against_support_map(
+        &cyl_at, &cyl, &cuboid_at, &cuboid, 10.0,
+    );
+
+    assert!(distance == 0.0);
+    assert!(proximity == Proximity::Intersecting);
+    assert!(contact.is_some());
+}
--- build/ncollide3d/tests/geometry/mod.rs
+++ build/ncollide3d/tests/geometry/mod.rs
@@ -1,6 +1,7 @@
 mod ball_ball_toi;
 mod ball_triangle_toi;
 mod contact;
+mod cylinder_cuboid_contact;
 mod epa3;
 mod time_of_impact3;
 mod trimesh_trimesh_toi;
