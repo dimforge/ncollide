45ff2d15efe3ab5199abb0615a56866d5de7c749
sebcrozet <developer@crozet.re>
2018-07-01 15:15:20 +0200

    First try to fix perpendicular raycast.

--- build/ncollide2d/tests/geometry/ray_cast.rs
+++ build/ncollide2d/tests/geometry/ray_cast.rs
@@ -1,25 +1,90 @@
-use na::{Isometry2, Point2, Vector2};
+use na::{self, Isometry2, Point2, Vector2};
+use ncollide2d::query::{Ray, RayCast};
 use ncollide2d::shape::Segment;
-use ncollide2d::query::{self, RayCast};
 
 #[test]
 fn issue_178_parallel_raycast() {
     let m1 = Isometry2::identity();
-    let ray = query::Ray::new(Point2::new(0.0, 0.0), Vector2::new(0.0, 1.0));
+    let ray = Ray::new(Point2::new(0.0, 0.0), Vector2::new(0.0, 1.0));
     let seg = Segment::new(Point2::new(2.0, 1.0), Point2::new(2.0, 0.0));
 
     let cast = seg.toi_with_ray(&m1, &ray, true);
-    println!("{:?}", cast);
     assert!(cast.is_none());
 }
 
 #[test]
 fn parallel_raycast() {
     let m1 = Isometry2::identity();
-    let ray = query::Ray::new(Point2::new(0.0, 0.0), Vector2::new(0.0, 1.0));
+    let ray = Ray::new(Point2::new(0.0, 0.0), Vector2::new(0.0, 1.0));
     let seg = Segment::new(Point2::new(2.0, 1.0), Point2::new(2.0, -1.0));
 
     let cast = seg.toi_with_ray(&m1, &ray, true);
-    println!("{:?}", cast);
     assert!(cast.is_none());
+}
+
+#[test]
+fn collinear_raycast_starting_on_segment() {
+    let m1 = Isometry2::identity();
+    let ray = Ray::new(Point2::new(0.0, 0.0), Vector2::new(0.0, 1.0));
+    let seg = Segment::new(Point2::new(0.0, 1.0), Point2::new(0.0, -1.0));
+
+    let cast = seg.toi_with_ray(&m1, &ray, true);
+    assert_eq!(cast, Some(0.0));
+}
+
+#[test]
+fn collinear_raycast_starting_bellow_segment() {
+    let m1 = Isometry2::identity();
+    let ray = Ray::new(Point2::new(0.0, -2.0), Vector2::new(0.0, 1.0));
+    let seg = Segment::new(Point2::new(0.0, 1.0), Point2::new(0.0, -1.0));
+
+    let cast = seg.toi_with_ray(&m1, &ray, true);
+    assert_eq!(cast, Some(1.0));
+}
+
+#[test]
+fn collinear_raycast_starting_above_segment() {
+    let m1 = Isometry2::identity();
+    let ray = Ray::new(Point2::new(0.0, 2.0), Vector2::new(0.0, 1.0));
+    let seg = Segment::new(Point2::new(0.0, 1.0), Point2::new(0.0, -1.0));
+
+    let cast = seg.toi_with_ray(&m1, &ray, true);
+    assert_eq!(cast, None);
+}
+
+#[test]
+fn perpendicular_raycast_starting_behind_sement() {
+    let segment = Segment::new(Point2::new(0.0f32, -10.0), Point2::new(0.0, 10.0));
+    let ray = Ray::new(Point2::new(-1.0, 0.0), Vector2::new(1.0, 0.0));
+    assert!(segment.intersects_ray(&na::one(), &ray));
+}
+
+#[test]
+fn perpendicular_raycast_starting_in_front_of_sement() {
+    let segment = Segment::new(Point2::new(0.0f32, -10.0), Point2::new(0.0, 10.0));
+    let ray = Ray::new(Point2::new(1.0, 0.0), Vector2::new(1.0, 0.0));
+    assert!(!segment.intersects_ray(&na::one(), &ray));
+}
+
+#[test]
+fn perpendicular_raycast_starting_on_segment() {
+    let segment = Segment::new(Point2::new(0.0f32, -10.0), Point2::new(0.0, 10.0));
+    let ray = Ray::new(Point2::new(0.0, 3.0), Vector2::new(1.0, 0.0));
+
+    let cast = segment.toi_with_ray(&na::one(), &ray, true);
+    assert_eq!(cast, Some(0.0));
+}
+
+#[test]
+fn perpendicular_raycast_starting_above_segment() {
+    let segment = Segment::new(Point2::new(0.0f32, -10.0), Point2::new(0.0, 10.0));
+    let ray = Ray::new(Point2::new(0.0, 11.0), Vector2::new(1.0, 0.0));
+    assert!(!segment.intersects_ray(&na::one(), &ray));
+}
+
+#[test]
+fn perpendicular_raycast_starting_bellow_segment() {
+    let segment = Segment::new(Point2::new(0.0f32, -10.0), Point2::new(0.0, 10.0));
+    let ray = Ray::new(Point2::new(0.0, -11.0), Vector2::new(1.0, 0.0));
+    assert!(!segment.intersects_ray(&na::one(), &ray));
 }
