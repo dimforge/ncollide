60780c3e8ccf2b3b1d10d564981e02330ba7de0b
sebcrozet <developer@crozet.re>
2018-10-07 12:20:55 +0200

    Use the correct environment variable for detecting travis rust version.

--- ci/bench.sh
+++ ci/bench.sh
@@ -2,7 +2,7 @@
 
 set -ev
 
-if [ "$RUST_VERSION" == "nightly" ]; then
+if [ "$TRAVIS_RUST_VERSION" == "nightly" ]; then
     cd $TRAVIS_BUILD_DIR/build/ncollide2d
     cargo bench
     cd $TRAVIS_BUILD_DIR/build/ncollide3d
