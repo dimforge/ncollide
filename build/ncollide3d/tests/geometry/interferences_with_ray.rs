026d97cf37984ab4629147482f15fdd34c98e6fe
Sebastian Waisbrot <seppo0010@gmail.com>
2018-12-16 11:00:36 -0300

    rustfmt

--- build/ncollide3d/tests/geometry/interferences_with_ray.rs
+++ build/ncollide3d/tests/geometry/interferences_with_ray.rs
@@ -1,8 +1,8 @@
 use std::f32;
 
-use na::{Isometry3, Translation3, UnitQuaternion, Vector3, Point3};
+use na::{Isometry3, Point3, Translation3, UnitQuaternion, Vector3};
 use ncollide3d::{
-    query::{Ray,RayCast},
+    query::{Ray, RayCast},
     shape::{Ball, ShapeHandle},
     world::{CollisionGroups, CollisionWorld, GeometricQueryType},
 };
@@ -21,26 +21,26 @@ fn interferences_with_ray() {
 
     let ray = Ray::new(Point3::new(0.0, 0.0, 0.0), Vector3::new(1.0, 1.0, 1.0));
 
-    assert!(ball.toi_with_ray(
-        &iso,
-        &ray,
-        true,
-    ).is_some());
+    assert!(ball.toi_with_ray(&iso, &ray, true,).is_some());
 
     let shape = ShapeHandle::new(ball);
     world.add(iso, shape.clone(), groups, query, ());
     world.update();
 
-    let num_collision_objects = world.collision_objects().into_iter().collect::<Vec<_>>().len();
+    let num_collision_objects = world
+        .collision_objects()
+        .into_iter()
+        .collect::<Vec<_>>()
+        .len();
     assert!(
-        num_collision_objects  == 1,
+        num_collision_objects == 1,
         format!("Expected 1 collision object, got {}", num_collision_objects),
     );
 
     let interferences = world.interferences_with_ray(&ray, &groups);
     let num_collisions = interferences.into_iter().collect::<Vec<_>>().len();
     assert!(
-        num_collisions  == 1,
+        num_collisions == 1,
         format!("Expected 1 collision, got {}", num_collisions),
     );
 }
