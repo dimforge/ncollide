ca9045886c1fcb8649fe2e4efc73b4de37fe0388
Aleksi Juvani <aleksi@aleksijuvani.com>
2019-05-09 12:54:52 +0300

    Improve triangle AABB performance

--- src/bounding_volume/aabb_support_map.rs
+++ src/bounding_volume/aabb_support_map.rs
@@ -1,10 +1,10 @@
 use crate::bounding_volume;
 use crate::bounding_volume::{HasBoundingVolume, AABB};
 use crate::math::Isometry;
-use na::RealField;
-use crate::shape::{Segment, Capsule};
+use crate::shape::{Capsule, Segment};
 #[cfg(feature = "dim3")]
-use crate::shape::{Cone, Cylinder, Triangle};
+use crate::shape::{Cone, Cylinder};
+use na::RealField;
 
 #[cfg(feature = "dim3")]
 impl<N: RealField> HasBoundingVolume<N, AABB<N>> for Cone<N> {
@@ -25,15 +25,6 @@ impl<N: RealField> HasBoundingVolume<N, AABB<N>> for Cylinder<N> {
 impl<N: RealField> HasBoundingVolume<N, AABB<N>> for Capsule<N> {
     #[inline]
     fn bounding_volume(&self, m: &Isometry<N>) -> AABB<N> {
-        bounding_volume::support_map_aabb(m, self)
-    }
-}
-
-#[cfg(feature = "dim3")]
-impl<N: RealField> HasBoundingVolume<N, AABB<N>> for Triangle<N> {
-    #[inline]
-    fn bounding_volume(&self, m: &Isometry<N>) -> AABB<N> {
-        // FIXME: optimize that
         bounding_volume::support_map_aabb(m, self)
     }
 }
