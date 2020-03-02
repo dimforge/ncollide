058c6590b79d82dd8827686365c9924c7ae80b7e
Sébastien Crozet <developer@crozet.re>
2020-03-02 14:55:37 +0100

    Remove old ci folder.

--- ci/build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-set -ev
-
-cd $TRAVIS_BUILD_DIR/build/ncollide2d
-cargo build
-cd $TRAVIS_BUILD_DIR/build/ncollide3d
-cargo build
--- ci/test.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-set -ev
-
-cd $TRAVIS_BUILD_DIR/build/ncollide2d
-cargo test
-cd $TRAVIS_BUILD_DIR/build/ncollide3d
-cargo test
